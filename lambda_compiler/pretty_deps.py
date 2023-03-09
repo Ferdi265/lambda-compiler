17e983d67b2b77ae09e127087e4465245c4518d6
Ferdinand Bachmann <theferdi265@gmail.com>
2023-03-09 11:07:30 +0100

    lambda_compiler: generate rules for compiling hlir2hlis (for large projects)

--- lambda_compiler/pretty_deps.py
+++ lambda_compiler/pretty_deps.py
@@ -12,8 +12,14 @@ def pretty_make_deps(crate_order: List[ModuleNamespace], outfile: str, output_di
     def get_hlir(mod: ModuleNamespace) -> str:
         return os.path.join(output_dir, mod.get_name() + ".hlir")
 
+    def get_hlis(mod: ModuleNamespace) -> str:
+        return os.path.join(output_dir, mod.get_name() + ".hlis")
+
     def get_mlir(mod: ModuleNamespace) -> str:
         return os.path.join(output_dir, mod.get_name() + ".mlir")
+
+    def get_llir(mod: ModuleNamespace, main: bool = False) -> str:
+        return os.path.join(output_dir, mod.get_name() + (".main" if main else "") + ".ll")
 
     all_mod_deps = []
     for mod in crate_order:
@@ -24,24 +30,26 @@ def pretty_make_deps(crate_order: List[ModuleNamespace], outfile: str, output_di
         lambda_src = get_lambda(mod_submod_deps[0])
         lambda_mods = list(map(get_lambda, mod_submod_deps[1:]))
 
-        hlir_src = os.path.join(output_dir, mod_name + ".hlir")
-        hlir_crate_deps = list(map(get_hlir, mod_crate_deps))
+        hlir_src = get_hlir(mod)
+        hlir_crate_deps = list(map(get_hlis, mod_crate_deps))
         print(f"{hlir_src}: {lambda_src} {' '.join(hlir_crate_deps + lambda_mods)} | {outfile}", end="\n\n", file=file)
 
-        mlir_src = os.path.join(output_dir, mod_name + ".mlir")
+        hlis_src = get_hlis(mod)
+        print(f"{hlis_src}: {hlir_src} | {outfile}", end="\n\n", file=file)
+
+        mlir_src = get_mlir(mod)
         mlir_crate_deps = list(map(get_mlir, mod_crate_deps))
         print(f"{mlir_src}: {hlir_src} {' '.join(mlir_crate_deps)} | {outfile}", end="\n\n", file=file)
 
-        llir_src = os.path.join(output_dir, mod_name + ".ll")
+        llir_src = get_llir(mod)
         print(f"{llir_src}: {mlir_src} | {outfile}", end="\n\n", file=file)
 
         all_mod_deps += list(map(get_lambda, mod_submod_deps))
 
-    crate_name = get_name(crate_order[0])
-    llir_main_src = os.path.join(output_dir, crate_name + ".main.ll")
-    mlir_main_src = os.path.join(output_dir, crate_name + ".mlir")
-    llir_crate_deps = list(map(get_mlir, crate_order[1:]))
-    print(f"{llir_main_src}: {mlir_main_src} {' '.join(llir_crate_deps)} | {outfile}", end="\n\n", file=file)
+    crate = crate_order[0]
+    llir_main_src = get_llir(crate, main=True)
+    llir_crate_deps = list(map(get_mlir, crate_order))
+    print(f"{llir_main_src}: {' '.join(llir_crate_deps)} | {outfile}", end="\n\n", file=file)
 
     print(f"{outfile}: {' '.join(all_mod_deps)}", end="\n\n", file=file)
 
