a399eb342a6b1ef9c8b204974f8c4a2e5f5688db
Ferdinand Bachmann <theferdi265@gmail.com>
2023-03-10 01:44:54 +0100

    lambda_compiler/legacy: remove mlir types and pretty printer

--- lambda_compiler/legacy/pretty_mlir.py
+++ lambda_compiler/legacy/pretty_mlir.py
@@ -1,5 +1,4 @@
 from .definitions import *
-from .parse_mlir import *
 import sys
 
 class PrettyMLIRError(Exception):
@@ -13,16 +12,10 @@ def pretty_mlir(prog: List[Statement], file: TextIO = sys.stdout):
         match stmt:
             case ExternCrate() as crate:
                 visit_extern_crate(crate)
-            case MImplementation() as impl:
-                visit_mlir_implementation(impl)
             case Implementation() as impl:
                 visit_implementation(impl)
-            case MInstance() as inst:
-                visit_mlir_instance(inst)
             case Instance() as inst:
                 visit_instance(inst)
-            case MInstanceDefinition() as inst_def:
-                visit_mlir_instance_definition(inst_def)
             case InstanceDefinition() as inst_def:
                 visit_instance_definition(inst_def)
             case _:
@@ -30,19 +23,6 @@ def pretty_mlir(prog: List[Statement], file: TextIO = sys.stdout):
 
     def visit_extern_crate(crate: ExternCrate):
         print(f"extern crate {crate.name};", file=file)
-
-    def visit_mlir_implementation(impl: MImplementation):
-        print(f"impl {impl.path.path}!{impl.path.lambda_id}!{impl.path.continuation_id} = ", end="", file=file)
-
-        match impl:
-            case MReturnImplementation() as impl:
-                print(f"{visit_literal(impl.value)};", file=file)
-            case MTailCallImplementation() as impl:
-                print(f"{visit_literal(impl.fn)} {visit_literal(impl.arg)};", file=file)
-            case MContinueCallImplementation() as impl:
-                print(f"{visit_literal(impl.fn)} {visit_literal(impl.arg)} -> {visit_literal(impl.next)};", file=file)
-            case _:
-                raise PrettyMLIRError(f"unexpected AST node encountered: {impl}")
 
     def visit_implementation(impl: Implementation):
         assert impl.arg_literal is None or impl.arg_literal == AnonymousLiteral(0)
@@ -59,22 +39,10 @@ def pretty_mlir(prog: List[Statement], file: TextIO = sys.stdout):
                 print(f"{visit_literal(impl.fn)} {visit_literal(impl.arg)} -> {visit_literal(impl.next)};", file=file)
             case _:
                 raise PrettyMLIRError(f"unexpected AST node encountered: {impl}")
-
-    def visit_mlir_instance(inst: MInstance):
-        impl = inst.impl
-        captures = " ".join(f"{i.path}%{i.inst_id}" for i in inst.captures)
-        print(f"inst {inst.path.path}%{inst.path.inst_id} = {impl.path}!{impl.lambda_id}!{impl.continuation_id}[{captures}];", file=file)
-
     def visit_instance(inst: Instance):
         impl = inst.impl
         captures = " ".join(f"{i.path}%{i.inst_id}" for i in inst.captures)
         print(f"inst {inst.path}%{inst.inst_id} = {impl.path}!{impl.lambda_id}!{impl.continuation_id}[{captures}];", file=file)
-
-    def visit_mlir_instance_definition(inst_def: MInstanceDefinition):
-        inst = inst_def.inst
-        is_public_str = "pub " if inst_def.is_public else ""
-        init_tag = " $$" if inst_def.needs_init else ""
-        print(f"{is_public_str}{inst_def.path} = {inst.path}%{inst.inst_id}{init_tag};", file=file)
 
     def visit_instance_definition(inst_def: InstanceDefinition):
         inst = inst_def.inst
@@ -90,14 +58,9 @@ def pretty_mlir(prog: List[Statement], file: TextIO = sys.stdout):
                 return f"{path}"
             case AnonymousLiteral(id):
                 return f"${id}"
-            case MImplementationLiteral(impl):
-                captures = " ".join(f"${id}" for id in impl.captures)
-                return f"{impl.path.path}!{impl.path.lambda_id}!{impl.path.continuation_id}[{captures}]"
             case ImplementationLiteral(impl):
                 captures = " ".join(f"${id}" for id in impl.anonymous_captures)
                 return f"{impl.path}!{impl.lambda_id}!{impl.continuation_id}[{captures}]"
-            case MInstanceLiteral(inst):
-                return f"{inst.path}%{inst.inst_id}"
             case InstanceLiteral(inst):
                 return f"{inst.path}%{inst.inst_id}"
             case _:
